7c4922e0ced1ee092892c7d65239dd9e08381ebc
Ali Ahsan <ahsan10041@gmail.com>
2023-05-28 23:44:58 +0200

    Added JavaDoc for ChessGameApplication

--- src/main/java/chessgame/ChessGameApplication.java
+++ src/main/java/chessgame/ChessGameApplication.java
@@ -9,8 +9,19 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
+/**
+ * The ChessGameApplication class is the entry point for the Chess Game application.
+ * It extends the JavaFX Application class and handles the initialization and startup of the application.
+ */
 public class ChessGameApplication extends Application {
 
+        /**
+         * The start method is called when the application is launched.
+         * It loads the startpage.fxml file, sets up the primary stage, and displays the main scene.
+         *
+         * @param primaryStage the primary stage of the application
+         * @throws IOException if an I/O error occurs while loading the startpage.fxml file
+         */
         @Override
         public void start (Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/startpage.fxml"));
