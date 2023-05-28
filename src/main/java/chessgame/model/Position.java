15f25d5bcfec216262b1c0274e78ea2fe2f914c8
Ali Ahsan <ahsan10041@gmail.com>
2023-05-28 17:24:58 +0200

    Added JavaDoc For Position Class

--- src/main/java/chessgame/model/Position.java
+++ src/main/java/chessgame/model/Position.java
@@ -1,7 +1,15 @@
 package chessgame.model;
 
+/**
+ * The Position class represents a position on a chess board.
+ * It consists of a row and column coordinate.
+ */
 public record Position(int row, int col) {
-
+    /**
+     * Returns a string representation of the Position object.
+     *
+     * @return a string representation of the Position object
+     */
     public String toString() {
         return String.format("(%d,%d)", row, col);
     }
