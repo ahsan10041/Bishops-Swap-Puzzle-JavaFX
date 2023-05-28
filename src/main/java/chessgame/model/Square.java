3ae68e9a0d9f7f680a531d1acfb8677a6670e5d3
Ali Ahsan <ahsan10041@gmail.com>
2023-05-28 17:48:08 +0200

    Added JavaDoc comments in Square Class

--- src/main/java/chessgame/model/Square.java
+++ src/main/java/chessgame/model/Square.java
@@ -1,7 +1,22 @@
 package chessgame.model;
 
+/**
+ * The Square enum represents the possible states of a chess square.
+ */
 public enum Square {
+
+    /**
+     * Represents an empty square on the chess board.
+     */
     NONE,
+
+    /**
+     * Represents a square occupied by a black chess piece.
+     */
     BLACK,
+
+    /**
+     * Represents a square occupied by a white chess piece.
+     */
     WHITE
 }
