8b3bd8a027e5c3d0c145e9e5d0ebfab52ff0c37c
Ali Ahsan <ahsan10041@gmail.com>
2023-05-29 00:09:55 +0200

    Added JavaDoc for StartPageController

--- src/main/java/chessgame/StartPageController.java
+++ src/main/java/chessgame/StartPageController.java
@@ -10,10 +10,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-
+/**
+ * The controller class for the Start Page of the chess game.
+ * Handles user interactions and navigation between scenes.
+ */
 public class StartPageController {
     private Stage stage;
     private Scene scene;
@@ -31,6 +33,14 @@ public class StartPageController {
 
     String PlayerName;
 
+
+    /**
+     * Switches to the game scene when the "Start Game" button is clicked.
+     * Validates the player name input and initializes the game.
+     *
+     * @param event The action event triggered by clicking the "Start Game" button.
+     * @throws IOException If an I/O error occurs while loading the game scene.
+     */
     public void SwitchToGame(ActionEvent event) throws IOException {
 
         if(playerNameTextField.getText().isBlank()){
@@ -49,6 +59,12 @@ public class StartPageController {
         }
     }
 
+    /**
+     * Switches to the high scores scene when the "High Scores" button is clicked.
+     *
+     * @param event The action event triggered by clicking the "High Scores" button.
+     * @throws IOException If an I/O error occurs while loading the high scores scene.
+     */
     public void showHighScores(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/highscores.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -58,11 +74,22 @@ public class StartPageController {
         stage.show();
     }
 
+    /**
+     * Exits the game when the "Exit Game" button is clicked.
+     *
+     * @param event The action event triggered by clicking the "Exit Game" button.
+     * @throws IOException If an I/O error occurs while closing the application window.
+     */
     public void exitGame(ActionEvent event) throws IOException {
         Stage stage = (Stage) exitGameButton.getScene().getWindow();
         stage.close();
     }
 
+    /**
+     * Returns the current player's game instance.
+     *
+     * @return The current player's game instance.
+     */
     public static Game getCurrentPlayer(){
         return game;
     }
